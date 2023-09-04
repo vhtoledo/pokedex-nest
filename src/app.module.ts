d5857502e6e945fb7062faeeab8d2bf5facd6e38
vhtoledo <querynetworking@gmail.com>
2023-09-04 09:39:55 -0300

    configuracion de global prefix api/v2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { join } from 'path'; // de node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
-      })
+      }),
+    PokemonModule
   ],
 })
 export class AppModule {}
