724f941b4756b3b1587a3de87b375ca7da7fef2b
vhtoledo <querynetworking@gmail.com>
2023-09-04 10:30:51 -0300

    creacion de eschema y models

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,9 +6,6 @@ async function bootstrap() {
 
   // configuracion de prefijo
   app.setGlobalPrefix('api/v2');
-
-  // referencia a nuestra bd
-  MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
   await app.listen(3001);
 }
