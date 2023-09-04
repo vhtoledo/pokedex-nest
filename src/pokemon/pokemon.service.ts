686885252a8097572c6f262206375059218c5c62
vhtoledo <querynetworking@gmail.com>
2023-09-04 11:24:49 -0300

    validar valores unicos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -73,5 +73,12 @@ export class PokemonService {
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
   }
-  
+
+  private handleExceptions( error: any ) {
+    if ( error.code === 11000 ) {
+      throw new BadRequestException(`Pokemon exists in db ${ JSON.stringify( error.keyValue ) }`);
+    }
+    console.log(error);
+    throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+  }
 }
