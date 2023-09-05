35b9d05c1906e5ea11add607a1a6e9f086e375ea
vhtoledo <querynetworking@gmail.com>
2023-09-05 12:21:37 -0300

    JOI - validationSchema, para las variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,14 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
 
     ConfigModule.forRoot({
       load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema
     }),
 
     ServeStaticModule.forRoot({
